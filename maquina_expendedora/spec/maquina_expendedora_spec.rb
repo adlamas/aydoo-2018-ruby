f5013e12b43de04c73e9a08ec9d8ed97227429e3
adlamas <alan.daniel.lamas@gmail.com>
2018-05-22 00:29:51 -0300

    test poner 2 de azucar al vaso pasado

--- maquina_expendedora/spec/maquina_expendedora_spec.rb
+++ maquina_expendedora/spec/maquina_expendedora_spec.rb
@@ -5,7 +5,6 @@ require_relative '../model/Vaso'
 describe "Test" do
 
   let(:un_vaso) { Vaso.new }
-
   subject { MaquinaExpendedoraDeBebidas.new }
 
   it 'deberia hacer cafe con leche con 10 de azucar' do
@@ -17,6 +16,26 @@ describe "Test" do
     expect(un_vaso.tiene_cafe?).to be_truthy
     expect(un_vaso.tiene_azucar?).to be_truthy
     expect(un_vaso.tiene_te?).to be_falsey
+  end
+
+=begin
+  it 'deberia hacer te con leche con 0 de azucar' do
+    cantidad_azucar = 0
+
+    subject.hacer_te_con_leche_con_n_de_azucar(un_vaso, cantidad_azucar)
+
+    expect(un_vaso.cantidad_de_azucar).to eq cantidad_azucar
+    expect(un_vaso.tiene_cafe?).to be_falsey
+    expect(un_vaso.tiene_azucar?).to be_falsey
+    expect(un_vaso.tiene_te?).to be_truthy
+  end
+=end
+
+  it "poner 2 de azucar al vaso" do
+    cantidad_azucar = 2
+    un_vaso.agregar_azucar(cantidad_azucar)
+    expect(un_vaso.cantidad_de_azucar).to eq 2
+
   end
 
 end
