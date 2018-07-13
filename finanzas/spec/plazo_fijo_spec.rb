a3b369fac9ca060a5cf459ee91b8d65f4da64a3b
adlamas <alan.daniel.lamas@gmail.com>
2018-07-13 00:09:43 -0300

    test devolver interes total por los dias de plazo

--- finanzas/spec/plazo_fijo_spec.rb
+++ finanzas/spec/plazo_fijo_spec.rb
@@ -6,20 +6,25 @@ describe 'Plazo Fijo' do
 
   it 'crear una inversion de tipo Plazo Fijo y devolver su monto' do
 
-    plazo_fijo = PlazoFijo.new(90, 10, 100000)
-    expect(plazo_fijo.monto).to eq 100000
+    plazo_fijo = PlazoFijo.new(90, 10.0, 100000.0)
+    expect(plazo_fijo.monto).to eq 100000.0
   end
 
   it 'crear una inversion de tipo Plazo Fijo y devolver su interes' do
 
-    plazo_fijo = PlazoFijo.new(90, 10, 100000)
-    expect(plazo_fijo.interes).to eq 10
+    plazo_fijo = PlazoFijo.new(90, 10.0, 100000.0)
+    expect(plazo_fijo.interes).to eq 10.0
   end
 
   it 'crear una inversion de tipo Plazo Fijo y devolver su plazo' do
 
-    plazo_fijo = PlazoFijo.new(90, 10, 100000)
+    plazo_fijo = PlazoFijo.new(90, 10.0, 100000.0)
     expect(plazo_fijo.plazo).to eq 90
+  end
+
+  it "devolver interes total por los dias de plazo" do
+    plazo_fijo = PlazoFijo.new(90, 10.00, 100000.00)
+    expect(plazo_fijo.interes_por_dias_de_plazo).to eq 2.5
   end
 
 end
