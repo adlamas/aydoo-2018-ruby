3937228509c4f7780cc9b61c8508b2289c266360
adlamas <alan.daniel.lamas@gmail.com>
2018-07-12 23:09:57 -0300

    test devolver interes de plazo fijo / devolver plazo de plazo fijo

--- finanzas/spec/plazo_fijo_spec.rb
+++ finanzas/spec/plazo_fijo_spec.rb
@@ -10,4 +10,16 @@ describe 'Plazo Fijo' do
     expect(plazo_fijo.monto).to eq 100000
   end
 
+  it 'crear una inversion de tipo Plazo Fijo y devolver su interes' do
+
+    plazo_fijo = PlazoFijo.new(90, 10, 100000)
+    expect(plazo_fijo.interes).to eq 10
+  end
+
+  it 'crear una inversion de tipo Plazo Fijo y devolver su plazo' do
+
+    plazo_fijo = PlazoFijo.new(90, 10, 100000)
+    expect(plazo_fijo.plazo).to eq 90
+  end
+
 end
