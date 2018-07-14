a1b1ffbba6e00b575cd28093b25c588d417c67f0
adlamas <alan.daniel.lamas@gmail.com>
2018-07-13 23:01:14 -0300

    test la obtencion de ganancia de un plazo fijo Precancelable cortando el plazo de 90, 10, 100000 debe ser de 1222.8

--- finanzas/model/plazo_fijo_precancelable.rb
+++ finanzas/model/plazo_fijo_precancelable.rb
@@ -10,7 +10,7 @@ class PlazoFijoPrecancelable < Inversion
     @interes = interes.round(4)
   end
 
-  def obtener_interes_por_dias_de_plazo()
+  def obtener_interes_por_dias_de_plazo_totales()
     porcentaje_de_anio = (365.0/@plazo)
     porcentaje_de_interes_por_dias = 100.0/porcentaje_de_anio
     interes_final_a_cobrar = ((porcentaje_de_interes_por_dias*@interes)/100.0 ).round(4)
@@ -18,8 +18,12 @@ class PlazoFijoPrecancelable < Inversion
     return interes_final_a_cobrar
   end
 
-  def obtener_ganancia()
-    return ((obtener_interes_por_dias_de_plazo() * @monto)/100)
+  def obtener_ganancia_a_fin_de_plazo()
+    return ((obtener_interes_por_dias_de_plazo_totales() * @monto)/100)
+  end
+
+  def obtener_ganancia_cortando_plazo()
+    return (((obtener_interes_por_dias_de_plazo_totales()/2) * @monto)/100)
   end
 
 end
--- finanzas/spec/plazo_fijo_precancelable_spec.rb
+++ finanzas/spec/plazo_fijo_precancelable_spec.rb
@@ -24,7 +24,13 @@ describe 'Plazo Fijo Precancelable' do
   it "la obtencion de ganancia de un plazo fijo Precancelable de 90, 10, 100000 debe ser de 2465.8" do
 
     plazo_fijo_precancelable = PlazoFijoPrecancelable.new(90, 10.0, 100000.0)
-    expect(plazo_fijo_precancelable.obtener_ganancia).to eq 2465.8
+    expect(plazo_fijo_precancelable.obtener_ganancia_a_fin_de_plazo).to eq 2465.8
+  end
+
+  it "la obtencion de ganancia de un plazo fijo Precancelable cortando el plazo de 90, 10, 100000 debe ser de 1222.8" do
+
+    plazo_fijo_precancelable = PlazoFijoPrecancelable.new(90, 10.0, 100000.0)
+    expect(plazo_fijo_precancelable.obtener_ganancia_cortando_plazo).to eq 1232.9
   end
 
 end
