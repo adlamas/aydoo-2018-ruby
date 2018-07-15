718876c066094cca6704dd9196ece0312e0461dd
adlamas <alan.daniel.lamas@gmail.com>
2018-07-15 17:36:41 -0300

    refactorizacion inversion

--- finanzas/model/inversion.rb
+++ finanzas/model/inversion.rb
@@ -3,23 +3,13 @@ require_relative 'impuesto'
 
 class Inversion
 
-  attr_accessor :monto, :impuesto
+  attr_accessor :monto
 
   def initialize(monto)
     if(monto < 0)
       raise ExcepcionMontoInvalido
     end
     @monto = monto.round(4)
-    @impuesto = Impuesto.new()
-  end
-
-  def agregar_impuesto(impuesto)
-    #@impuesto = impuesto
-  end
-
-  def obtener_impuesto_a_ganancia_de_inversion(inversor)
-    tasa = impuesto.calcular_tasa(inversor, inversor.ganancias_brutas)
-    return tasa
   end
 
 end
