4c52c4e774f8475f3f969ac4bce85e01b4cda9a7
adlamas <alan12394@hotmail.com>
2018-07-17 23:10:31 -0300

    cambio en el mensaje de excepciones

--- finanzas/excepciones/excepcion_monto_invalido.rb
+++ finanzas/excepciones/excepcion_monto_invalido.rb
@@ -1,7 +1,7 @@
 
 class ExcepcionMontoInvalido < Exception
 
-  def initialize(msg="El monto de la inversion siempre debe ser mayor a cero")
+  def initialize(msg="El monto de cada inversion siempre debe ser mayor a cero")
     puts msg
   end
 end
